ebf78d62be108eea2b890251c199d2ddff012b9e
arktal <101146331+gpollet@users.noreply.github.com>
2022-05-20 12:37:06 +0200

    Used Prettier to add some space in code

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const cartSection = document.getElementById("cart__items");
 
@@ -14,174 +14,190 @@ let cartItemImageAlt;
 let productName;
 let productColor;
 let productPrice;
-let productQuantity = []
-let displayPrice = []
-let cartContent = '';
+let productQuantity = [];
+let displayPrice = [];
+let cartContent = "";
 let i = 0;
 
 // Récupère les données de l'API, puis 1/ compare l'ID du panier en localstorage avec l'id des produits de l'API 2/ retourne les propriétés des produits trouvés dans l'API et le localstorage dans le innerHTML puis 3/ ajoute un event listener sur les input "Qté"
 function displayCart() {
   fetch("http://localhost:3000/api/products")
     .then((response) => {
-      return response.json()
+      return response.json();
     })
     .then((APIProductList) => {
       while (i !== cart.length) {
-        const matchingProduct = APIProductList.find(product => product._id == cart[i].id)
-        APIProductList.forEach(productInList => {
+        const matchingProduct = APIProductList.find(
+          (product) => product._id == cart[i].id
+        );
+        APIProductList.forEach((productInList) => {
           if (matchingProduct) {
-            cartItemImage = `${matchingProduct.imageUrl}`
-            cartItemImageAlt = `${matchingProduct.altTxt}`
-            productName = `${matchingProduct.name}`
-            productColor = `${cart[i].color}`
-            productPrice = `${matchingProduct.price}`
+            cartItemImage = `${matchingProduct.imageUrl}`;
+            cartItemImageAlt = `${matchingProduct.altTxt}`;
+            productName = `${matchingProduct.name}`;
+            productColor = `${cart[i].color}`;
+            productPrice = `${matchingProduct.price}`;
           }
-        })
-        createInnerContent()
-        displayPrice.push(productPrice)
-        i++
+        });
+        createInnerContent();
+        displayPrice.push(productPrice);
+        i++;
       }
       cartSection.innerHTML += cartContent;
-      eventListener()
-      getCartTotal()
-      formInputValidation()
-    })
+      eventListener();
+      getCartTotal();
+      formInputValidation();
+    });
 }
 
 // Crée un nouveau bloc article pour chaque produit du panier
 const createInnerContent = function () {
-  cartContent += `<article class="cart__item" data-id="${cart[i].id}" data-color="${productColor}"><div class="cart__item__img"><img src="${cartItemImage}" alt="${cartItemImageAlt}"></div><div class="cart__item__content"><div class="cart__item__content__description"><h2>${productName}</h2><p>${productColor}</p><p>${productPrice} €</p></div><div class="cart__item__content__settings"><div class="cart__item__content__settings__quantity"><p>Qté : </p><input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cart[i].quantity}"></div><div class="cart__item__content__settings__delete"><p class="deleteItem">Supprimer</p></div></div></div></article>`
-}
+  cartContent += `<article class="cart__item" data-id="${cart[i].id}" data-color="${productColor}"><div class="cart__item__img"><img src="${cartItemImage}" alt="${cartItemImageAlt}"></div><div class="cart__item__content"><div class="cart__item__content__description"><h2>${productName}</h2><p>${productColor}</p><p>${productPrice} €</p></div><div class="cart__item__content__settings"><div class="cart__item__content__settings__quantity"><p>Qté : </p><input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cart[i].quantity}"></div><div class="cart__item__content__settings__delete"><p class="deleteItem">Supprimer</p></div></div></div></article>`;
+};
 
+displayCart();
 
-displayCart()
-
-const totalQuantityElement = document.getElementById("totalQuantity")
-const displayedTotal = document.getElementById("totalPrice")
-let removeButton
-let quantityInputField
-let getParentArticle
-let updatedProduct
+const totalQuantityElement = document.getElementById("totalQuantity");
+const displayedTotal = document.getElementById("totalPrice");
+let removeButton;
+let quantityInputField;
+let getParentArticle;
+let updatedProduct;
 
 // Ajoute un event listener sur les <input> quantité de chaque produit et leur bouton "Supprimer"; active la fonction getCartTotal si la valeur d'un input change, ou la fonction removeFromCart si un clic sur "Supprimer"; ajuste les quantités individuelles de chaque produit sur la page
 const eventListener = function () {
-  quantityInputField = document.querySelectorAll(".cart__item__content__settings__quantity > .itemQuantity")
-  quantityInputField.forEach(element => {
-    element.setAttribute('value', element.value)
-    productQuantity += element.value
-    element.addEventListener("change", pushLocalStorageQuantity)
+  quantityInputField = document.querySelectorAll(
+    ".cart__item__content__settings__quantity > .itemQuantity"
+  );
+  quantityInputField.forEach((element) => {
+    element.setAttribute("value", element.value);
+    productQuantity += element.value;
+    element.addEventListener("change", pushLocalStorageQuantity);
   });
-  removeButton = document.querySelectorAll(".deleteItem")
-  removeButton.forEach(element => {
-    element.addEventListener("click", removeFromCart)
-  })
-}
+  removeButton = document.querySelectorAll(".deleteItem");
+  removeButton.forEach((element) => {
+    element.addEventListener("click", removeFromCart);
+  });
+};
 
 const getUnitQuantities = function () {
-  quantityInputField.setAttribute('value', element.value)
-}
+  quantityInputField.setAttribute("value", element.value);
+};
 
 // Quand la quantité dans les <input> change, calcule le total depuis les quantités affichées et le prix des produits récupérés depuis l'API dans la fonction displayCart
 const getCartTotal = function () {
-  let cartTotalPrice = new Number()
+  let cartTotalPrice = new Number();
   let cartTotalQuantity = new Number();
-  let i = 0
+  let i = 0;
   while (i !== quantityInputField.length) {
-    cartTotalQuantity += Number(quantityInputField[i].value)
-    cartTotalPrice += displayPrice[i] * quantityInputField[i].value
-    i++
+    cartTotalQuantity += Number(quantityInputField[i].value);
+    cartTotalPrice += displayPrice[i] * quantityInputField[i].value;
+    i++;
   }
-  totalQuantityElement.textContent = cartTotalQuantity
-  displayedTotal.textContent = cartTotalPrice
-  eventListener()
-}
+  totalQuantityElement.textContent = cartTotalQuantity;
+  displayedTotal.textContent = cartTotalPrice;
+  eventListener();
+};
 
 // Compare l'id et la couleur de l'élément 'article' avec ceux contenus dans le panier en localstorage, pour renvoyer au caller (fonction pour supprimer un produit du panier, ou pour mettre à jour la quantité) le produit concerné
 const getProductToUpdate = function () {
   let parentArticleDataset = {
     color: getParentArticle.dataset.color,
-    id: getParentArticle.dataset.id
-  }
-  updatedProduct = cart.find(product => product.color == parentArticleDataset.color && product.id == parentArticleDataset.id)
+    id: getParentArticle.dataset.id,
+  };
+  updatedProduct = cart.find(
+    (product) =>
+      product.color == parentArticleDataset.color &&
+      product.id == parentArticleDataset.id
+  );
   return updatedProduct;
-}
+};
 
 // Trouve l'index du produit dans cart pour le supprimer, le retirer du DOM et forcer un recalcul du total
 const removeFromCart = function () {
-  getParentArticle = this.closest("article")
-  getProductToUpdate()
+  getParentArticle = this.closest("article");
+  getProductToUpdate();
   if (updatedProduct) {
-    const indexOfRemovedProduct = cart.indexOf(updatedProduct)
-    const removeProduct = cart.splice(indexOfRemovedProduct, 1)
-    getParentArticle.remove()
+    const indexOfRemovedProduct = cart.indexOf(updatedProduct);
+    const removeProduct = cart.splice(indexOfRemovedProduct, 1);
+    getParentArticle.remove();
     localStorage.setItem("cart", JSON.stringify(cart));
-    eventListener()
-    getCartTotal()
+    eventListener();
+    getCartTotal();
   }
-}
+};
 
 // Trouve l'index du produit dans cart pour mettre à jour sa quantité, puis force un recalcul du total
 const pushLocalStorageQuantity = function () {
-  getParentArticle = this.closest("article")
-  getProductToUpdate()
+  getParentArticle = this.closest("article");
+  getProductToUpdate();
   if (updatedProduct) {
-    const indexOfUpdatedProduct = cart.indexOf(updatedProduct)
-    updatedProduct.quantity = parseInt(quantityInputField[indexOfUpdatedProduct].value)
+    const indexOfUpdatedProduct = cart.indexOf(updatedProduct);
+    updatedProduct.quantity = parseInt(
+      quantityInputField[indexOfUpdatedProduct].value
+    );
     localStorage.setItem("cart", JSON.stringify(cart));
-    eventListener()
-    getCartTotal()
-    convertCartToArray()
+    eventListener();
+    getCartTotal();
+    convertCartToArray();
   }
-}
+};
 
-const formInputs = document.querySelectorAll(".cart__order__form__question > input")
-const formInputsErrors = document.querySelectorAll(".cart__order__form__question > p")
-const orderButton = document.getElementById('order')
-
+const formInputs = document.querySelectorAll(
+  ".cart__order__form__question > input"
+);
+const formInputsErrors = document.querySelectorAll(
+  ".cart__order__form__question > p"
+);
+const orderButton = document.getElementById("order");
 
 // Ajoute un eventListener à chaque élément champs du formulaire
 const formInputValidation = function () {
-  formInputs.forEach(inputField => {
-    inputField.addEventListener("change", checkInput)
+  formInputs.forEach((inputField) => {
+    inputField.addEventListener("change", checkInput);
   });
-  orderButton.addEventListener("click", sendCartAndInput)
+  orderButton.addEventListener("click", sendCartAndInput);
 };
 
-
-const nameCriterias = /^[a-zç]+[a-zç ,.'-]+$/i
-const emailCriterias = /[a-z0-9!#%&'*+/=^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
-let validationStatus = [undefined, undefined, undefined,undefined,undefined]
-let errorName
+const nameCriterias = /^[a-zç]+[a-zç ,.'-]+$/i;
+const emailCriterias =
+  /[a-z0-9!#%&'*+/=^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+let validationStatus = [undefined, undefined, undefined, undefined, undefined];
+let errorName;
 
 // Détecte le champs qui a été modifié, vérifie s'il répond aux critères de saisie définis, et si false modifie la balise html d'erreur correspondante. Si la saisie est à nouveau valide après modification, retire le message d'erreur.
 const checkInput = function (targetElement) {
-  let i = 0
+  let i = 0;
   while (i < 5) {
     if (this.id == `${formInputs.item(i).name}`) {
-      if (this.id == 'firstName' || this.id == 'lastName' || this.id == 'city') {
-        validationStatus[i] = nameCriterias.test(this.value)
-      } else if (this.id == 'email') {
-        validationStatus[i] = emailCriterias.test(this.value)
-      } else if (this.id == 'address') {
+      if (
+        this.id == "firstName" ||
+        this.id == "lastName" ||
+        this.id == "city"
+      ) {
+        validationStatus[i] = nameCriterias.test(this.value);
+      } else if (this.id == "email") {
+        validationStatus[i] = emailCriterias.test(this.value);
+      } else if (this.id == "address") {
         if (formInputs[i].value.length > 5) {
-          validationStatus[i] = true
+          validationStatus[i] = true;
         } else {
-          validationStatus[i] = false
+          validationStatus[i] = false;
         }
       }
-      errorName = document.getElementById(`${formInputsErrors.item(i).id}`)
+      errorName = document.getElementById(`${formInputsErrors.item(i).id}`);
       if (validationStatus[i] == false) {
-        errorName.textContent = "Vérifiez votre saisie"
+        errorName.textContent = "Vérifiez votre saisie";
       } else {
-        errorName.textContent = ""
+        errorName.textContent = "";
       }
-      break
+      break;
     }
-    i++
+    i++;
   }
-}
+};
 
-let contact
+let contact;
 
 // Sauvegarde le contenu des champs du formulaire dans une variable contact
 const saveInputForm = function () {
@@ -191,58 +207,63 @@ const saveInputForm = function () {
     address: `${formInputs[2].value}`,
     city: `${formInputs[3].value}`,
     email: `${formInputs[4].value}`,
-  }
-}
+  };
+};
 
-const products = []
+const products = [];
 
 // Extrait le contenu du panier en localstorage et le sauvegarde dans une variable products
 const convertCartToArray = function () {
-  cart.forEach(product => {
-    cart.find(product => product.id)
-    products.push(product.id)
+  cart.forEach((product) => {
+    cart.find((product) => product.id);
+    products.push(product.id);
     /* Si besoin de supprimer les id doublons car couleur pas prise en compte par API, décommenter ci-dessous et y intégrer contenu de la fonction convertCartToArray
     if (products.includes(product.id) == false) {
     }
     */
   });
-}
+};
 
-let orderProducts
+let orderProducts;
 
 // Regroupe le contenu du panier et le contenu des champs du formulaire dans un unique object
 const mergeInputs = function () {
-  convertCartToArray()
-  saveInputForm()
+  convertCartToArray();
+  saveInputForm();
   orderProducts = {
     contact,
-    products
-  }
-}
+    products,
+  };
+};
 
 // Vérifie que tous les champs du formulaire sont valides, et si oui : envoie le contenu du panier, redirige vers la page de confirmation correspondant à l'orderid, et vide le localstorage
 function sendCartAndInput(event) {
-  event.preventDefault()
-  if (!validationStatus.includes(false) && !validationStatus.includes(undefined)) {
-    mergeInputs()
+  event.preventDefault();
+  if (
+    !validationStatus.includes(false) &&
+    !validationStatus.includes(undefined)
+  ) {
+    mergeInputs();
     fetch("http://localhost:3000/api/products/order", {
-        method: "POST",
-        body: JSON.stringify(orderProducts),
-        headers: {
-          "Content-Type": "application/json",
-        }
-      })
+      method: "POST",
+      body: JSON.stringify(orderProducts),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
       .then((response) => {
-        return response.json()
+        return response.json();
       })
       .then((response) => {
         localStorage.clear();
-        window.location.href = `confirmation.html?order=${response.orderId}`
+        window.location.href = `confirmation.html?order=${response.orderId}`;
       })
       .catch((error) => {
-        console.log(error)
-      })
+        console.log(error);
+      });
   } else {
-    alert("Le formulaire n'a pas pu être validé. Tous les champs sont-ils correctement remplis?")
+    alert(
+      "Le formulaire n'a pas pu être validé. Tous les champs sont-ils correctement remplis?"
+    );
   }
 }
